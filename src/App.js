60df361b4cde1d75839d6d43bffc5ec4a9014b4d
Fernanda Luna <ferluna.rm@gmail.com>
2023-02-28 11:37:08 -0600

    1. Layout Components (#1)
    * Layout components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import ListItemsPage from "./pages/layout-components/list-items";
+import ModalsPage from "./pages/layout-components/modal";
+
+// Split-screen components
+import { SplitScreenPage } from "./pages/layout-components/split-screen";
+
+// Lists and list items
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <SplitScreenPage /> */}
+      {/* <ListItemsPage /> */}
+      <ModalsPage />
     </div>
   );
 }
